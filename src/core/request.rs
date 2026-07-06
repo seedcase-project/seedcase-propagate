90955e00d389fddb8dc70f76e4224be7fb04fcda
Luke W. Johnston <lwjohnst@gmail.com>
2026-07-06 11:31:33 +0200

    feat: ✨ add `data_package` type to Request type

--- src/core/request.rs
+++ src/core/request.rs
@@ -19,6 +19,11 @@ pub struct Request {
     pub project: Project,
     /// Details about the [`Requester`] who is making the request for the data.
     pub requester: Requester,
+    /// Details about the data package ([`Package`]) that the request is for.
+    /// Mostly used to provide a reference to the original data package, as
+    /// well as to provide a way for the Owner to track which version of the
+    /// data package the request is for.
+    pub data_package: Package,
     /// The specific resources, columns, and rows to make as [`Subsets`] for the request.
     pub subsets: Subsets,
 }
@@ -48,6 +53,17 @@ pub struct Requester {
     pub name: String,
     /// The email of the Requester, for when contact is needed.
     pub email: String,
+}
+
+pub struct Package {
+    /// The name of the data package, which is mainly used as a (human-readable)
+    /// identifier.
+    pub name: String,
+    /// The version of the data package, which is used to determine which
+    /// version of the data package is being displayed when creating the
+    /// request. Can be used to determine which version the request was
+    /// created for.
+    pub version: String,
 }
 
 /// Details about what resources, columns, and rows are being requested as a
