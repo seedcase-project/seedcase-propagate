73b1be512cd834c4ca24fac4f8b92ce09d944635
Luke W. Johnston <lwjohnst86@users.noreply.github.com>
2026-07-09 13:44:37 +0200

    docs: :pencil2: edits from review
    Co-authored-by: Signe Kirk Brødbæk <40836345+signekb@users.noreply.github.com>

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,6 @@
 // `build` and `create-request` CLI commands contain some functionality from
 // these modules, but (will likely) be specific to the CLI commands themselves.
-// Which is why they aren't in `core`.
+// Therefore, they aren't included here.
 pub mod check;
 pub mod metadata;
 pub mod subset;
