e93a367ad4552f7811b340c2a4492695af296318
Luke W. Johnston <lwjohnst@gmail.com>
2026-08-07 21:12:34 +0200

    refactor: ♻️ update structs based on changes in design

--- src/core/request.rs
+++ src/core/request.rs
@@ -1,31 +1,35 @@
 //! Functions and types for making requests for subsets of the data package.
 
+use chrono::{DateTime, Utc};
+use semver::Version;
+
 /// A type that maps the contents of `request.yaml`.
-pub struct Request {
-    /// The initial date when the request was created.
-    pub data_created: String,
-    /// The date when the request was last modified, mainly used when requesting
-    /// an update to a previously submitted request.
-    pub data_modified: String,
-    /// The number of times that a request has been updated, mainly to use for
-    /// versioning of the request. It is optional because it requires manual
-    /// incrementing that must be done by the Requester and checked by the
-    /// Owner.
-    pub iteration: Option<u8>,
-    /// The explanation for why the specific data is needed for the given
-    /// [`Project`].
-    pub motivation: String,
-    /// Details about the research [`Project`] that will use the requested data.
-    pub project: Project,
+pub struct RequestMetadata {
+    /// Details about the [`Request`] overall.
+    pub request: Request,
     /// Details about the [`Requester`] who is making the request for the data.
     pub requester: Requester,
-    /// Details about the data package ([`Package`]) that the request is for.
+    /// Details about the research [`Project`] that will use the requested data.
+    pub project: Project,
+    /// Details about the data package ([`DataPackage`]) that the request is for.
     /// Mostly used to provide a reference to the original data package, as
     /// well as to provide a way for the Owner to track which version of the
     /// data package the request is for.
-    pub data_package: Package,
-    /// The specific resources, columns, and rows to make as [`Subsets`] for the request.
+    pub data_package: DataPackage,
+    /// The specific resources, columns, and rows to make as [`Subsets`] for the
+    /// request.
     pub subsets: Subsets,
+}
+
+pub struct Request {
+    /// The initial datetime when the request was created.
+    pub datetime_created: DateTime<Utc>,
+    /// The datetime when the request was last modified, mainly used when
+    /// requesting an update to a previously submitted request.
+    pub datetime_modified: DateTime<Utc>,
+    /// The explanation for why the specific data is needed for the given
+    /// [`Project`].
+    pub motivation: String,
 }
 
 /// The details about the research project that the request is for. This is used
@@ -33,7 +37,8 @@ pub struct Request {
 /// assess the request, the motivation, and whether the request is appropriate
 /// for the given project.
 pub struct Project {
-    /// The name of the project in identifier format (lowercase, `-` for spaces).
+    /// The name of the project in identifier format (lowercase, `-` for
+    /// spaces).
     pub name: String,
     /// The title of the project in human-readable format.
     pub title: String,
@@ -55,7 +60,7 @@ pub struct Requester {
     pub email: String,
 }
 
-pub struct Package {
+pub struct DataPackage {
     /// The name of the data package, which is mainly used as a (human-readable)
     /// identifier.
     pub name: String,
@@ -63,7 +68,7 @@ pub struct Package {
     /// version of the data package is being displayed when creating the
     /// request. Can be used to determine which version the request was
     /// created for.
-    pub version: String,
+    pub version: Version,
 }
 
 /// Details about what resources, columns, and rows are being requested as a
