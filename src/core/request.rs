0f19c4b61dd3b2caa1291bf1375ecebb32efe6ca
Luke W. Johnston <lwjohnst@gmail.com>
2026-08-13 17:16:45 +0200

    refactor: 🔥 remove unneeded nesting

--- src/core/request.rs
+++ src/core/request.rs
@@ -9,25 +9,35 @@ use semver::Version;
 pub struct Request {
     /// The initial datetime when the request was created.
     pub datetime_created: DateTime<Utc>,
+
     /// The datetime when the request was last modified, mainly used when
     /// requesting an update to a previously submitted request.
     pub datetime_modified: DateTime<Utc>,
+
     /// The explanation for why the specific data is needed for the given
     /// [`Project`].
     pub motivation: String,
+
     /// Details about the [`Requester`] who is making the request for the data.
     pub requester: Requester,
+
     /// Details about the research [`Project`] that will use the requested data.
     pub project: Project,
+
     /// Details about the data package ([`DataPackage`]) that the request is
     /// for. Mostly used to provide a reference to the original data
     /// package, as well as to provide a way for the Owner to track which
     /// version of the data package the request is for.
     pub data_package: DataPackage,
-    /// The specific rows from the resources requested, as [`Rows`].
-    pub rows: Rows,
-    /// The specific columns from the resources requested, as [`Columns`].
-    pub columns: Columns,
+
+    /// Row-level subsets of resources (as a vector of hashmaps), with the
+    /// resource name as the first item and the where conditions (as a
+    /// vector) as the second.
+    pub rows: Vec<HashMap<String, Vec<Where>>>,
+
+    /// Column-level subsets of resources (as a vector of hashmaps) that
+    /// contains the specific columns to include (as a vector of strings).
+    pub columns: Vec<HashMap<String, Vec<String>>>,
 }
 
 /// The details about the research project that the request is for. This is used
@@ -38,8 +48,10 @@ pub struct Project {
     /// The name of the project in identifier format (lowercase, `-` for
     /// spaces).
     pub name: String,
+
     /// The title of the project in human-readable format.
     pub title: String,
+
     /// A description of the project. Should contain enough information for the
     /// Owner to assess the request. Ideally should have the research
     /// question(s) and general analysis methodology included.
@@ -54,6 +66,7 @@ pub struct Requester {
     /// The name of the Requester, for when the Owner needs to reach out to the
     /// Requester.
     pub name: String,
+
     /// The email of the Requester, for when contact is needed.
     pub email: String,
 }
@@ -62,6 +75,7 @@ pub struct DataPackage {
     /// The name of the data package, which is mainly used as a (human-readable)
     /// identifier.
     pub name: String,
+
     /// The version of the data package, which is used to determine which
     /// version of the data package is being displayed when creating the
     /// request. Can be used to determine which version the request was
@@ -69,20 +83,12 @@ pub struct DataPackage {
     pub version: Version,
 }
 
-/// Details about what rows in the resources are being requested as a subset of
-/// the data package.
-pub struct Rows {
-    /// Row-level subsets of resources (as a vector of hashmaps), with the
-    /// resource name as the first item and the where conditions (as a
-    /// vector) as the second.
-    pub subsets: Vec<HashMap<String, Vec<Where>>>,
-}
-
 /// Details about how the rows will be kept from the resources.
 pub enum Where {
     /// The logic conditions when **all** are true that determine which rows are
     /// kept, as a [`WhereCondition`] struct.
     All(Vec<WhereCondition>),
+
     /// The logic conditions when **any of them** true that determine which rows
     /// are kept, as a [`WhereCondition`] struct.
     Any(Vec<WhereCondition>),
@@ -93,19 +99,14 @@ pub enum Where {
 pub struct WhereCondition {
     /// The column to apply the logic condition to.
     pub column: String,
+
     // TODO: This should probably be converted into an enum of allowed operators.
     /// The logic operator to apply between the `column` and `value`.
     pub operator: String,
+
     /// The value to compare against the row value.
     pub value: Option<String>,
+
     /// Whether to do the inverse of the logical condition.
     pub not: Option<bool>,
-}
-
-/// Details about what columns in resources are being requested as a subset of
-/// the data package.
-pub struct Columns {
-    /// Column-level subsets of resources (as a vector of hashmaps) that
-    /// contains the specific columns to include (as a vector of strings).
-    pub subsets: Vec<HashMap<String, Vec<String>>>,
 }
