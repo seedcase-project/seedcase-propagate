ddd72b7c66850b29116666e784ee3f0a1c14b3cb
Luke W. Johnston <lwjohnst@gmail.com>
2026-08-13 11:49:39 +0200

    refactor: ♻️ update types based on design

--- src/core/request.rs
+++ src/core/request.rs
@@ -1,38 +1,29 @@
 //! Functions and types for making requests for subsets of the data package.
 
-/// Details about what resources, columns, and rows are being requested as a
-/// subset of the data package.
-pub struct Subsets {
-    /// Information on what rows to keep from the resources, as a
-    /// [`RowSubset`].
-    pub rows: Vec<RowsSubset>,
-    /// Information on what columns to keep from the resources, as a
-    /// [`ColumnsSubset`].
-    pub columns: Vec<ColumnsSubset>,
-}
+use std::collections::HashMap;
 
-/// Details about which columns will be selected from the resources.
-pub struct ColumnsSubset {
-    pub resource: String,
-    /// The columns of the resource that are being requested as a subset.
-    pub columns: Vec<String>,
+/// Details about what rows in the resources are being requested as a subset of
+/// the data package.
+pub struct Rows {
+    /// Row-level subsets of resources (as a vector of hashmaps), with the
+    /// resource name as the first item and the where conditions (as a
+    /// vector) as the second.
+    pub subsets: Vec<HashMap<String, Vec<Where>>>,
 }
 
 /// Details about how the rows will be kept from the resources.
-pub struct RowsSubset {
-    /// The name of the resource that is being requested as a subset.
-    pub resource: String,
+pub enum Where {
     /// The logic conditions when **all** are true that determine which rows are
-    /// kept, as a [`RowWhere`] struct.
-    pub where_all: Option<Vec<RowWhere>>,
+    /// kept, as a [`WhereSubset`] struct.
+    All(WhereSubset),
     /// The logic conditions when **any of them** true that determine which rows
-    /// are kept, as a [`RowWhere`] struct.
-    pub where_any: Option<Vec<RowWhere>>,
+    /// are kept, as a [`WhereSubset`] struct.
+    Any(WhereSubset),
 }
 
 /// The individual row conditions that will be applied to an individual
 /// resource.
-pub struct RowWhere {
+pub struct WhereSubset {
     /// The column to apply the logic condition to.
     pub column: String,
     // TODO: This should probably be converted into an enum of allowed operators.
@@ -42,4 +33,12 @@ pub struct RowWhere {
     pub value: String,
     /// Whether to do the inverse of the logical condition.
     pub not: Option<bool>,
+}
+
+/// Details about what columns in resources are being requested as a subset of
+/// the data package.
+pub struct Columns {
+    /// Column-level subsets of resources (as a vector of hashmaps) that
+    /// contains the specific columns to include (as a vector of strings).
+    pub subsets: Vec<HashMap<String, Vec<String>>>,
 }
