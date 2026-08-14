54f6fa3e4d82ae92d50792a9335cf85881c9b538
Luke W. Johnston <lwjohnst@gmail.com>
2026-08-14 13:47:34 +0200

    refactor: 🏷️ add deserialization and tests

--- src/core/request.rs
+++ src/core/request.rs
@@ -1,17 +1,21 @@
 //! Functions and types for making requests for subsets of the data package.
 
+use serde::Deserialize;
 use std::collections::HashMap;
 
 use chrono::{DateTime, Utc};
 use semver::Version;
 
 /// A type that maps the contents of `request.yaml`.
+#[derive(Debug, Deserialize)]
 pub struct Request {
     /// The initial datetime when the request was created.
+    #[serde(rename = "datetime-created")]
     pub datetime_created: DateTime<Utc>,
 
-    /// The datetime when the request was last modified, mainly used when
+    /// The request's datetime when it was last modified, mainly used when
     /// requesting an update to a previously submitted request.
+    #[serde(rename = "datetime-modified")]
     pub datetime_modified: DateTime<Utc>,
 
     /// The explanation for why the specific data is needed for the given
@@ -28,22 +32,24 @@ pub struct Request {
     /// for. Mostly used to provide a reference to the original data
     /// package, as well as to provide a way for the Owner to track which
     /// version of the data package the request is for.
+    #[serde(rename = "data-package")]
     pub data_package: DataPackage,
 
-    /// Row-level subsets of resources (as a vector of hashmaps), with the
+    /// Row-level subsets of resources (as a hashmaps), with the
     /// resource name as the first item and the where conditions (as a
     /// vector) as the second.
-    pub rows: Vec<HashMap<String, Vec<Where>>>,
+    pub rows: HashMap<String, Vec<Where>>,
 
-    /// Column-level subsets of resources (as a vector of hashmaps) that
+    /// Column-level subsets of resources (as a hashmaps) that
     /// contains the specific columns to include (as a vector of strings).
-    pub columns: Vec<HashMap<String, Vec<String>>>,
+    pub columns: HashMap<String, Vec<String>>,
 }
 
 /// The details about the research project that the request is for. This is used
 /// to provide context to the requested data, so that the Owner can adequately
 /// assess the request, the motivation, and whether the request is appropriate
 /// for the given project.
+#[derive(Debug, Deserialize)]
 pub struct Project {
     /// The name of the project in identifier format (lowercase, `-` for
     /// spaces).
@@ -62,6 +68,7 @@ pub struct Project {
 /// to both to verify where/who the request is coming from and to provide
 /// contact information for the Owner to reach out to the Requester if there are
 /// any questions or concerns about the request.
+#[derive(Debug, Deserialize)]
 pub struct Requester {
     /// The name of the Requester, for when the Owner needs to reach out to the
     /// Requester.
@@ -71,6 +78,9 @@ pub struct Requester {
     pub email: String,
 }
 
+/// Details about the data package that the request is asking for a data subset
+/// from.
+#[derive(Debug, Deserialize)]
 pub struct DataPackage {
     /// The name of the data package, which is mainly used as a (human-readable)
     /// identifier.
@@ -84,18 +94,22 @@ pub struct DataPackage {
 }
 
 /// Details about how the rows will be kept from the resources.
+#[derive(Debug, Deserialize)]
 pub enum Where {
     /// The logic conditions when **all** are true that determine which rows are
     /// kept, as a [`WhereCondition`] struct.
+    #[serde(rename = "all")]
     All(Vec<WhereCondition>),
 
     /// The logic conditions when **any of them** true that determine which rows
     /// are kept, as a [`WhereCondition`] struct.
+    #[serde(rename = "any")]
     Any(Vec<WhereCondition>),
 }
 
 /// The individual row conditions that will be applied to an individual
 /// resource.
+#[derive(Debug, Deserialize)]
 pub struct WhereCondition {
     /// The column to apply the logic condition to.
     pub column: String,
@@ -109,4 +123,80 @@ pub struct WhereCondition {
 
     /// Whether to do the inverse of the logical condition.
     pub not: Option<bool>,
+}
+
+#[cfg(test)]
+mod tests {
+    // To import all code from above in this file.
+    use super::*;
+    use serde_saphyr;
+
+    #[test]
+    fn test_deserialising_correctly() {
+        // `r#` means "raw string literal", to allow using `"` without escaping.
+        let test_request_yaml = r#"
+datetime-modified: "2026-07-06T01:45:34Z"
+datetime-created: "2026-07-04T01:44:34Z"
+motivation: |
+  We would like access to metabolic and block variables to evaluate
+  our hypothesis regarding ...
+
+requester:
+  name: "First Last"
+  email: "TEXT"
+
+project:
+  name: "metabolic-cost"
+  title: "Metabolic cost estimation"
+  description: |
+    Our project investigates the gas exchange during metabolism
+    with the aim to determine ...
+
+data-package:
+  name: "example-seed-beetle"
+  version: "0.5.1"
+
+columns:
+  metabolic-rate:
+    - "strain"
+    - "activity"
+  biometrics:
+  ids:
+
+rows:
+  metabolic-rate:
+    - all:
+      - column: "block"
+        operator: "="
+        value: "block1"
+    - any:
+      - column: "block1"
+        operator: "="
+        value: "10"
+        not: true
+    - any:
+      - column: "block2"
+        operator: "="
+        value: "10"
+        not: true
+"#;
+
+        let config: Result<Request, _> = serde_saphyr::from_str(test_request_yaml);
+        assert!(config.is_ok())
+    }
+
+    #[test]
+    fn test_serde_fail_not_all_present() {
+        let test_request_yaml = r#"
+datetime-modified: "2026-07-06T01:45:34Z"
+datetime-created: "2026-07-04T01:44:34Z"
+motivation: |
+  We would like access to metabolic and block variables to evaluate
+  our hypothesis regarding ...
+"#;
+
+        let config: Result<Request, _> = serde_saphyr::from_str(test_request_yaml);
+
+        assert!(config.is_err())
+    }
 }
