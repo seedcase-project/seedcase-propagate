3696a3760605949b8df7b870a84758e4a3c47c11
Daniel Mazhari-Jensen <danmaz94@gmail.com>
2026-08-31 08:26:06 +0200

    feat: ✨ add command arguments, doc strings, and default values
    respons to review

--- src/cli.rs
+++ src/cli.rs
@@ -8,21 +8,61 @@ pub mod create_request;
 pub mod subset;
 
 #[derive(Debug, Args)]
-pub struct Build;
+pub struct BuildArgs {
+    /// Path to the source directory.
+    #[arg(long, default_value = "datapackage.json")]
+    pub source: String,
+
+    /// Directory where the built output should be written.
+    #[arg(long, default_value = "docs/requests/")]
+    pub output_dir: String,
+}
 
 #[derive(Debug, Args)]
-pub struct Check;
+pub struct CheckArgs{
+    /// Path to the instructions request.yaml file.
+    #[arg(long)]
+    pub request: String,
+
+    /// Path to the datapackage.json file.
+    #[arg(short, long, default_value = "datapackage.json")]
+    pub source: String,
+}
 
 #[derive(Debug, Args)]
-pub struct CreateRequest;
+pub struct CreateRequestArgs{
+    /// Path to the datapackage.json file.
+    #[arg(short, long, default_value = "datapackage.json")]
+    pub source: String,
+}
 
 #[derive(Debug, Args)]
-pub struct Subset;
+pub struct SubsetArgs {
+  /// Path to the instructions request.yaml file.
+  #[arg()]
+  pub request: String,
+
+  /// Path to the datapackage.json file.
+  #[arg(short, long, default_value = "datapackage.json")]
+  pub source: String,
+
+  /// Directory where the subsetted output should be written.
+  #[arg(short, long, default_value = "subset/")] // TODO: actually subset/requester-project-name
+  pub output_dir: String,
+
+  /// Show the subsetting plan without executing it.
+  #[arg(short = 'p', long, default_value = "false")]
+  pub show_plan: bool,
+}
 
 #[derive(Debug, Subcommand)] // the enum of commands
 pub enum Commands {
-    Build(Build), // Commands can be a Build variant, and that variant contains a Build value.
-    Check(Check),
-    CreateRequest(CreateRequest),
-    Subset(Subset),
+    /// Build and set up the request web app for including in static websites.
+    Build(BuildArgs),
+    /// Validate the compatibility between request.yaml and datapackage.json.
+    Check(CheckArgs),
+    /// Interactively create a request.yaml file from source metadata.
+    CreateRequest(CreateRequestArgs),
+    /// Produce a subset based on the request.yaml and your metadata.
+    Subset(SubsetArgs),
 }
