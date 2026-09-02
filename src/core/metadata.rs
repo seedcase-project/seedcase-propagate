f098fc9048957d9b8329fcb6b4f83309692e03c4
Daniel Mazhari-Jensen <danmaz94@gmail.com>
2026-09-02 13:01:42 +0200

    feat: ✨ first test and implementation of `read_package_metadata()`
    added serde_json and tempfile to dependecies.

--- src/core/metadata.rs
+++ src/core/metadata.rs
@@ -1,9 +1,12 @@
 //! Library functionality for interacting with the metadata of a data package.
 
 use std::error::Error;
+use serde::Deserialize;
+use std::path::PathBuf;
 
 /// Top-level representation of the metadata of a data package. Contains only
 /// the fields from the Data Package spec that are relevant to Propagate.
+#[derive(Debug, Deserialize)]
 pub struct Package {
     /// The data package version used to determine which
     /// version of the data package is being displayed when creating the
@@ -16,6 +19,7 @@ pub struct Package {
 /// Represents the resource(s) in the data package. A resource is a single data
 /// file or collection of related data files within a data package. Resources
 /// can be different formats of data, such as Parquet, images, or audio files.
+#[derive(Debug, Deserialize)]
 pub struct Resource {
     /// The resource name (no spaces) used as an identifier.
     pub name: String,
@@ -31,22 +35,25 @@ pub struct Resource {
 
 /// Contributor or author information for the data package. This is used when
 /// displaying who to send the request to.
+#[derive(Debug, Deserialize)]
 pub struct Contributor {
     /// The name of the contributor.
-    pub name: String,
+    pub title: String,
     /// The email address of the contributor.
     pub email: String,
     /// The role of the contributor in the data package. This is only used to
     /// display contributors who are contact persons (corresponding authors)
     /// like the owner or manager.
-    pub role: Option<String>,
+    pub roles: Option<Vec<String>>,
 }
 
 /// The schema for the resource containing the column information. Only relevant
 /// for tabular data.
+#[derive(Debug, Deserialize)]
 pub struct Schema {
     /// The resource columns. Only relevant for resources in tabular format.
     /// Called `fields` in the Data Package spec.
+    #[serde(rename = "fields")]
     pub columns: Vec<Column>,
     /// The primary key for the resource.
     pub primary_key: Option<Vec<String>>,
@@ -62,12 +69,14 @@ pub struct Schema {
 }
 
 /// A column within a resource. Called `field` in the Data Package spec.
+#[derive(Debug, Deserialize)]
 pub struct Column {
     /// The column name (no spaces) used as an identifier.
     pub name: String,
     /// The column title (human formatted) used for display purposes.
-    pub title: String,
+    pub title: Option<String>,
     /// The column data type.
+    #[serde(rename = "type")]
     pub column_type: ColumnType,
     /// The column value constraints, e.g. minimum, maximum, or allowed values.
     pub constraints: Option<Constraints>,
@@ -79,6 +88,8 @@ pub struct Column {
 // practice.
 /// The supported column data types from the metadata file. Also matches
 /// what's allowed in Parquet files (our default format).
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum ColumnType {
     String,
     Integer,
@@ -92,6 +103,7 @@ pub enum ColumnType {
 
 /// Represents a foreign key relationship between two resources in the data
 /// package.
+#[derive(Debug, Deserialize)]
 pub struct ForeignKey {
     /// The column(s) in the current resource that form the foreign key.
     pub columns: Vec<String>,
@@ -104,6 +116,7 @@ pub struct ForeignKey {
 
 /// The column constraints, i.e. the minimum and maximum values, as well as
 /// allowed values.
+#[derive(Debug, Deserialize)]
 pub struct Constraints {
     /// The minimum allowed value for a column. The type of the minimum value
     /// depends on the type of the column.
@@ -118,6 +131,7 @@ pub struct Constraints {
 }
 
 /// The allowed extreme value for a column (e.g. max or min).
+#[derive(Debug, Deserialize)]
 pub enum Extreme {
     /// The allowed value for a column with values as integers (numbers
     /// without a decimal point).
@@ -136,10 +150,20 @@ pub enum Extreme {
 #[allow(clippy::needless_pass_by_value)]
 pub enum PackageSource {
     // TODO: May need to use e.g. `Path` or `PathBuf`, depends on what the `open` functions need.
-    Path(String),
+    Path(PathBuf),
     Https(String),
     GitHub(String),
 }
+
+
+//#[derive(Debug, thiserror::Error)]
+//pub enum MetadataError {
+//    #[error("failed to read metadata file: {0}")]
+//    Io(#[from] std::io::Error),
+
+//    #[error("invalid datapackage.json: {0}")]
+//    Json(#[from] serde_json::Error),
+//}
 
 /// Reads and parses a data package's metadata file into a `Package` struct.
 ///
@@ -155,7 +179,7 @@ pub enum PackageSource {
 /// exist) or if the metadata file is malformed (e.g., a `datapackage.json` file
 /// that doesn't contain parsable JSON).
 #[allow(unused_variables)]
-pub fn read_package(source: &PackageSource) -> Result<Package, Box<dyn Error>> {
+pub fn read_package_metadata(source: &PackageSource) -> Result<Package, Box<dyn Error>> {
     // Box holds an unknown number of errors known only at runtime.
     // Open the metadata from the source locations
     // We'll have to either make custom errors or make use of error packages
@@ -173,8 +197,24 @@ pub fn read_package(source: &PackageSource) -> Result<Package, Box<dyn Error>> {
     // Read the JSON contents of the file as an instance of `Package`.
     // let package: Package = read_from_json(package)?;
     // Ok(package)
-    todo!("Planned")
+    // todo!("Planned")
+    match source {
+      PackageSource::Path(path) => {
+        let contents = std::fs::read_to_string(path)?;
+        let package: Package = serde_json::from_str(&contents)?;
+        Ok(package)
+      }
+
+      PackageSource::Https(_) => {
+        todo!("HTTPS not yet supported")
+      }
+
+      PackageSource::GitHub(_) => {
+        todo!("GitHub not yet supported")
+      }
+    }
 }
+
 
 /// An example of a datapackage.json following the Data Package standard.
 /// Used for testing read and write functions.
@@ -245,3 +285,37 @@ pub const EXAMPLE_DATAPACKAGE_JSON: &str = r##"
   ]
 }
 "##;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_package_struct_deserialization() {
+        let package: Package =
+            serde_json::from_str(EXAMPLE_DATAPACKAGE_JSON).unwrap();
+
+        assert_eq!(package.version.as_deref(), Some("0.1.0"));
+        assert_eq!(package.resources.len(), 1);
+        assert_eq!(package.resources[0].name, "patients");
+    }
+
+    #[test]
+    fn test_read_package_metadata_using_path_input() {
+        use std::io::Write;
+        // see https://rust-exercises.com/advanced-testing/05_filesystem_isolation/02_tempfile.html
+        // for my design choices around tempfile::NamedTempFile
+
+        let mut file = tempfile::NamedTempFile::new().unwrap();
+       file.write_all(EXAMPLE_DATAPACKAGE_JSON.as_bytes()).unwrap();
+
+        let source = PackageSource::Path(file.path().to_path_buf());
+
+        let package = read_package_metadata(&source).unwrap();
+
+        assert_eq!(package.version.as_deref(), Some("0.1.0"));
+        assert_eq!(package.resources.len(), 1);
+        assert_eq!(package.resources[0].name, "patients");
+        assert_eq!(package.resources[0].title.as_deref(), Some("Patients Data"));
+    }
+}
