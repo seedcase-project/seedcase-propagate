572d5f076d612c61539bb7d7c79ce9ec07d6fe0a
Joel Ostblom <joelostblom@users.noreply.github.com>
2026-09-04 10:49:16 +0200

    Apply batched suggestions from code review
    Co-authored-by: Luke W. Johnston <lwjohnst86@users.noreply.github.com>

--- src/core/request.rs
+++ src/core/request.rs
@@ -340,9 +340,8 @@ pub fn read_request(path: &str) -> Result<Request, Box<dyn Error>> {
 ///
 /// # Errors
 ///
-/// Returns `Error` if the file cannot be created (e.g., no sufficient
-/// permissions or if the request file is malformed (i.e., it doesn't contain
-/// parsable YAML).
+/// Returns `Error` if the file cannot be created (e.g., not correct
+/// permissions).
 #[allow(unused_variables)]
 pub fn write_request(request: &Request, path: &str) -> Result<PathBuf, Box<dyn Error>> {
     // May not end up using `Error`, but this is pseudocode for now.
